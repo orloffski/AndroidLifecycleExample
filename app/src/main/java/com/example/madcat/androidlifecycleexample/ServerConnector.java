6b7c7c1a455df627990a04f97135320e1969808a
MadCat <orloffski@gmail.com>
2018-05-16 21:37:48 +0300

    Rev 0.3 add Lifecycle.Event.ON_ANY listener

--- app/src/main/java/com/example/madcat/androidlifecycleexample/ServerConnector.java
+++ app/src/main/java/com/example/madcat/androidlifecycleexample/ServerConnector.java
@@ -2,6 +2,7 @@ package com.example.madcat.androidlifecycleexample;
 
 import android.arch.lifecycle.Lifecycle;
 import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.OnLifecycleEvent;
 import android.util.Log;
 
@@ -15,5 +16,10 @@ public class ServerConnector implements LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     public void disconnect(){
         Log.d("Test", "server disconnected");
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    void onAny(LifecycleOwner source, Lifecycle.Event event) {
+        Log.d("Test", "Event: " + event.name() + " from source: " + source.toString());
     }
 }
